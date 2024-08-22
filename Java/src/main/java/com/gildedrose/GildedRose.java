9a811682fb49b5ae176963795f875b37d113722a
Dsouza <divya.a.dsouza@accenture.com>
2024-08-22 11:08:37 +0300

    Removed unused code

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -9,43 +9,6 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            switch (item.name) {
-                case "Aged Brie":
-                    increaseQuality(item);
-                    item.sellIn--;
-                    checkSellInToIncrement(item, 0);
-                    break;
-
-                case "Sulfuras, Hand of Ragnaros":
-                    //"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-                    break;
-
-                case "Backstage passes to a TAFKAL80ETC concert":
-                    increaseQuality(item);
-                    checkSellInToIncrement(item, 11);
-                    checkSellInToIncrement(item, 6);
-                    item.sellIn--;
-
-                    if (item.sellIn < 0)
-                        item.quality = 0;
-                    break;
-
-                default:
-                    boolean conjured = item.name.equals("Conjured Mana Cake");
-                    decreaseQuality(item);
-                    if (conjured)
-                        decreaseQuality(item);
-                    item.sellIn--;
-
-                    if (item.sellIn < 0)
-                        decreaseQualityToZero(item);
-                    break;
-            }
-        }
-    }
-
-    public void updateQuality_v1() {
-        for (Item item : items) {
             boolean incQualityItems = item.name.equals("Aged Brie")
                 || item.name.equals("Backstage passes to a TAFKAL80ETC concert");
             boolean conjured = item.name.equals("Conjured Mana Cake");
